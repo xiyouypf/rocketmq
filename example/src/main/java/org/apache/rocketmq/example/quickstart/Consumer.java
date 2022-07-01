1d60dcf1750b75e4c3c54ddabc50b4ea9abb4262
ypfyang <ypfyang@tencent.com>
2022-07-01 14:52:31 +0800

    RocketMQ启动代码

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -33,46 +33,19 @@ public class Consumer {
 
     public static void main(String[] args) throws InterruptedException, MQClientException {
 
-        /*
-         * Instantiate with specified consumer group name.
-         */
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(CONSUMER_GROUP);
 
-        /*
-         * Specify name server addresses.
-         * <p/>
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         * consumer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
-         */
-        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-//        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-
-        /*
-         * Specify where to start in case the specific consumer group is a brand-new one.
-         */
         consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 
-        /*
-         * Subscribe one more topic to consume.
-         */
         consumer.subscribe(TOPIC, "*");
 
-        /*
-         *  Register callback to execute on arrival of messages fetched from brokers.
-         */
+        consumer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
         consumer.registerMessageListener((MessageListenerConcurrently) (msg, context) -> {
-            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
+//            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msg);
+            System.out.println(msg);
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
-        /*
-         *  Launch the consumer instance.
-         */
         consumer.start();
 
         System.out.printf("Consumer Started.%n");
--- example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Producer.java
@@ -38,77 +38,18 @@ public class Producer {
 
     public static void main(String[] args) throws MQClientException, InterruptedException {
 
-        /*
-         * Instantiate with a producer group name.
-         */
         DefaultMQProducer producer = new DefaultMQProducer(PRODUCER_GROUP);
-
-        /*
-         * Specify name server addresses.
-         *
-         * Alternatively, you may specify name server addresses via exporting environmental variable: NAMESRV_ADDR
-         * <pre>
-         * {@code
-         *  producer.setNamesrvAddr("name-server1-ip:9876;name-server2-ip:9876");
-         * }
-         * </pre>
-         */
-        // Uncomment the following line while debugging, namesrvAddr should be set to your local address
-//        producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
-
-        /*
-         * Launch the instance.
-         */
+        producer.setNamesrvAddr(DEFAULT_NAMESRVADDR);
         producer.start();
 
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             try {
-
-                /*
-                 * Create a message instance, specifying topic, tag and message body.
-                 */
                 Message msg = new Message(TOPIC /* Topic */,
                     TAG /* Tag */,
                     ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
                 );
 
-                /*
-                 * Call send message to deliver message to one of brokers.
-                 */
                 SendResult sendResult = producer.send(msg);
-                /*
-                 * There are different ways to send message, if you don't care about the send result,you can use this way
-                 * {@code
-                 * producer.sendOneway(msg);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a synchronize way, you can use this send method
-                 * {@code
-                 * SendResult sendResult = producer.send(msg);
-                 * System.out.printf("%s%n", sendResult);
-                 * }
-                 */
-
-                /*
-                 * if you want to get the send result in a asynchronize way, you can use this send method
-                 * {@code
-                 *
-                 *  producer.send(msg, new SendCallback() {
-                 *  @Override
-                 *  public void onSuccess(SendResult sendResult) {
-                 *      // do something
-                 *  }
-                 *
-                 *  @Override
-                 *  public void onException(Throwable e) {
-                 *      // do something
-                 *  }
-                 *});
-                 *
-                 *}
-                 */
 
                 System.out.printf("%s%n", sendResult);
             } catch (Exception e) {
